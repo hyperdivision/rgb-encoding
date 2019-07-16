9f88b228b474ccafcf2f29ee6b1e014a940fdf48
Christophe Diederichs <chris.d.itunes@gmail.com>
2019-07-16 11:53:52 +0200

    verification moved to separate module

--- contract.js
+++ contract.js
@@ -48,32 +48,3 @@ function encodingLength (contract) {
   length += body.encodingLength(contract)
   return length
 }
-
-function verify (contract) {
-  // 1. Checking commitment transaction publishing the contract
-  // need to make tx_provider
-  let issueTX = tx_provider(contract.header.issuance_utxo.txid)
-
-  // 1.1. Checking commitment transaction script to be corresponding to the actual RGB
-  // contract
-  let vout = contract.header.issuance_utxo.vout
-  // ISSUE -> cast as usize necessary in JS? 32bit pointer?
-  // let vout_u = vout as usize
-
-  if (issueTX.output[vout].script_pubkey !== contract.get_script()) {
-    throw new Error('RGB-contract script does not correspond to transaction script.')
-  }
-
-  // 2. Checking header consistency
-  header.verify(contract)
-
-  // 3. Checkin body consistency
-  body.verify(contract)
-
-  return true
-}
-
-function validate (contract, proof) {
-  header.validate(contract, proof)
-  body.validate(contract, proof)
-}
